c725018cae4ad0b3d9ee23b4320ecd6bb48a33d2
chlxogur <83434398+chlxogur@users.noreply.github.com>
2024-06-19 15:06:07 +0900

    nevermind
    pitcheranalyze.py를 조금 보기 좋게 코드를 다듬어봤다. 다른건 달라진거 딱히 없음.

--- pitcheranalyze.py
+++ pitcheranalyze.py
@@ -2,31 +2,27 @@ from calculateColumnsForModel import numberToRoleName
 import pandas as pd
 
 def pitcheranalyze(dataset):
-    for i in range(5):
-        dataset.drop([f"Blue_{numberToRoleName(i)}_championDamageShare", f"Red_{numberToRoleName(i)}_championDamageShare"], axis=1, inplace=True)   # championdamageshare 다 지우고
-        dataset[f"{numberToRoleName(i)}_wardsScorePerTimeDiff"] = dataset[f"Blue_{numberToRoleName(i)}_wardsScorePerTime"] - dataset[f"Red_{numberToRoleName(i)}_wardsScorePerTime"]
-        dataset.drop([f"Blue_{numberToRoleName(i)}_wardsScorePerTime", f"Red_{numberToRoleName(i)}_wardsScorePerTime"], axis=1, inplace=True)
-
-    dataset.drop(["Blue_Spt_kda", f"Red_Spt_kda"], axis=1, inplace=True)
-    dataset.drop(["Blue_Spt_killsPerTime", f"Red_Spt_killsPerTime"], axis=1, inplace=True)
-    dataset.drop(["Blue_Spt_deathsPerTime", f"Red_Spt_deathsPerTime"], axis=1, inplace=True)
-    dataset["Spt_assistsPerTimeDiff"] = dataset[f"Blue_Spt_assistsPerTime"] - dataset[f"Red_Spt_assistsPerTime"]
-    dataset.drop(["Blue_Spt_creepScorePerTime", f"Red_Spt_creepScorePerTime"], axis=1, inplace=True)
-    dataset.drop(["Blue_Spt_goldEarnedPerTime", f"Red_Spt_goldEarnedPerTime"], axis=1, inplace=True)
     for i in range(4):
-        
         dataset[f"{numberToRoleName(i)}_kdaDiff"] = dataset[f"Blue_{numberToRoleName(i)}_kda"] - dataset[f"Red_{numberToRoleName(i)}_kda"]
-        #dataset[f"{numberToRoleName(i)}_championDamageShareDiff"] = dataset[f"Blue_{numberToRoleName(i)}_championDamageShare"] - dataset[f"Red_{numberToRoleName(i)}_championDamageShare"]
         dataset[f"{numberToRoleName(i)}_killsPerTimeDiff"] = dataset[f"Blue_{numberToRoleName(i)}_killsPerTime"] - dataset[f"Red_{numberToRoleName(i)}_killsPerTime"]
         dataset[f"{numberToRoleName(i)}_deathsPerTimeDiff"] = dataset[f"Blue_{numberToRoleName(i)}_deathsPerTime"] - dataset[f"Red_{numberToRoleName(i)}_deathsPerTime"]
         dataset[f"{numberToRoleName(i)}_assistsPerTimeDiff"] = dataset[f"Blue_{numberToRoleName(i)}_assistsPerTime"] - dataset[f"Red_{numberToRoleName(i)}_assistsPerTime"]
         dataset[f"{numberToRoleName(i)}_creepScorePerTimeDiff"] = dataset[f"Blue_{numberToRoleName(i)}_creepScorePerTime"] - dataset[f"Red_{numberToRoleName(i)}_creepScorePerTime"]
-        
         dataset[f"{numberToRoleName(i)}_goldEarnedPerTimeDiff"] = dataset[f"Blue_{numberToRoleName(i)}_goldEarnedPerTime"] - dataset[f"Red_{numberToRoleName(i)}_goldEarnedPerTime"]
+
+    dataset["Spt_assistsPerTimeDiff"] = dataset[f"Blue_Spt_assistsPerTime"] - dataset[f"Red_Spt_assistsPerTime"]
+    
+    for i in range(5):
+        dataset[f"{numberToRoleName(i)}_wardsScorePerTimeDiff"] = dataset[f"Blue_{numberToRoleName(i)}_wardsScorePerTime"] - dataset[f"Red_{numberToRoleName(i)}_wardsScorePerTime"]
         dataset.drop([f"Blue_{numberToRoleName(i)}_kda", f"Red_{numberToRoleName(i)}_kda"], axis=1, inplace=True)
         dataset.drop([f"Blue_{numberToRoleName(i)}_creepScorePerTime", f"Red_{numberToRoleName(i)}_creepScorePerTime"], axis=1, inplace=True)
         dataset.drop([f"Blue_{numberToRoleName(i)}_goldEarnedPerTime", f"Red_{numberToRoleName(i)}_goldEarnedPerTime"], axis=1, inplace=True)
+        dataset.drop([f"Blue_{numberToRoleName(i)}_wardsScorePerTime", f"Red_{numberToRoleName(i)}_wardsScorePerTime"], axis=1, inplace=True)
+        dataset.drop([f"Blue_{numberToRoleName(i)}_championDamageShare", f"Red_{numberToRoleName(i)}_championDamageShare"], axis=1, inplace=True)   # championdamageshare 다 지우고
 
+    dataset.drop(["Blue_Spt_killsPerTime", f"Red_Spt_killsPerTime"], axis=1, inplace=True)
+    dataset.drop(["Blue_Spt_deathsPerTime", f"Red_Spt_deathsPerTime"], axis=1, inplace=True)
+    
 
     dataset["teamWinrateDiff"] = dataset["Blue_Winrate"] - dataset["Red_Winrate"]
     dataset["teamGoldDiff"] = dataset["Blue_GoldDiff"] - dataset["Red_GoldDiff"]
